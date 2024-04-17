edf6f91cd1862a2807fccdca53a39f58ef3ed873
Careful-Zebra <vtiamvt@gmail.com>
2024-04-17 13:49:26 -0700

    fixed a minor raycasting error

--- Assets/Scripts/TrafficLightManager.cs
+++ Assets/Scripts/TrafficLightManager.cs
@@ -15,8 +15,8 @@ public class TrafficLightManager : MonoBehaviour
 
     #region Private Variables
 
-    //current state of light (0 is red, 1 is green, 2 is amber)
-    private int state;
+    //list of current states of light (0 is red, 1 is green, 2 is amber) [index 0 is N, index 1 is E, index 2 is S, index 3 is W]
+    private int[] state;
 
     //the timer used to change light
     private float countdown;
@@ -27,7 +27,7 @@ public class TrafficLightManager : MonoBehaviour
 
     void Start()
     {
-        state = 0;
+        state[0] = 0;
         countdown = m_ChangeTime;
         gameObject.GetComponent<Renderer>().material.color = Color.red;
     }
@@ -37,30 +37,33 @@ public class TrafficLightManager : MonoBehaviour
         countdown = countdown - Time.deltaTime;
         if (countdown < 0)
         {
-            state = (state + 1) % 3;
+            state[0] = (state[0] + 1) % 3;
 
             Renderer sr = gameObject.GetComponent<Renderer>();
 
             //set colour
-            if (state == 0)
+            if (state[0] == 0)
             {
                 sr.material.color = Color.red;
-            } else if (state == 1)
+                countdown = m_ChangeTime;
+            } else if (state[0] == 1)
             {
                 sr.material.color = Color.green;
+                countdown = m_ChangeTime;
             } else
             {
                 sr.material.color = Color.yellow;
+                countdown = m_ChangeTime / 2;
             }
 
-            countdown = m_ChangeTime;
+            
         }
     }
 
     public int GetState()
     {
         //again, 0 is red, 1 is green, 2 is amber
-        return state;
+        return state[0];
     }
 
     public string Type()
